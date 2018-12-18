8e492a424fe50e768b6285062d0ca2f8f7088bd0
gourav <gourav.hartron@gmail.com>
2018-12-18 15:18:08 +0530

    menu chal gya

--- client/src/main/webapp/app/entities/restaurantService/menu/menu-detail.component.ts
+++ client/src/main/webapp/app/entities/restaurantService/menu/menu-detail.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DishesService } from 'app/entities/restaurantService/dishes';
+import { IDishes } from 'app/shared/model/restaurantService/dishes.model';
 
 import { IMenu } from 'app/shared/model/restaurantService/menu.model';
+import * as _ from 'underscore';
 
 @Component({
     selector: 'jhi-menu-detail',
@@ -10,6 +12,10 @@ import { IMenu } from 'app/shared/model/restaurantService/menu.model';
 })
 export class MenuDetailComponent implements OnInit {
     menu: IMenu;
+    dishes: IDishes[] = [];
+    dessertDishes: IDishes[] = [];
+    starterDishes: IDishes[] = [];
+    maincourseDishes: IDishes[] = [];
 
     constructor(private activatedRoute: ActivatedRoute, private dishService: DishesService) {}
 
@@ -17,12 +23,29 @@ export class MenuDetailComponent implements OnInit {
         this.activatedRoute.data.subscribe(({ menu }) => {
             this.menu = menu;
             this.dishService.dishByMenuId(this.menu.id).subscribe(res => {
-                console.log(res.body);
+                this.dishes = res.body;
+                this.funcGroupBy();
             });
         });
     }
 
     previousState() {
         window.history.back();
+    }
+    myfunc(evt: any) {
+        console.log(evt);
+    }
+
+    funcGroupBy() {
+        const data = _.groupBy(this.dishes, 'dishType');
+        console.log(data);
+
+        this.dessertDishes = data.Dessert;
+        this.starterDishes = data.Starter;
+        this.maincourseDishes = data.MainCourse;
+
+        console.log(this.dessertDishes);
+        console.log(this.starterDishes);
+        console.log(this.dessertDishes);
     }
 }
